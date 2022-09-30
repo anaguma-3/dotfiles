ad5bde5f472b3ff750153a417af53cf5ce12ebdc
Takumi Yoshida <umibooose@gmail.com>
2022-09-30 22:50:32 +0900

    [update]Change the theme from p10k to starship

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,7 +1,7 @@
 # ----------------------------
 # Theme
 # ----------------------------
-zinit ice depth=1; zinit light romkatv/powerlevel10k
+# zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 # ----------------------------
 # Completions
